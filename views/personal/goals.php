f37eaba26cd647b9a39a4a2d19b0a6bb5e76a029
Andrey <bikeway@yandex.ru>
2017-04-24 01:05:05 +0300

    with model

--- views/personal/goals.php
+++ views/personal/goals.php
@@ -25,7 +25,7 @@ echo $this->render('_panel.php');
 						<div class="output">
 							<div class="output-header">
 								 <div class="mygoals-name-div">
-									<h2 class="mdlst-h2t-goals withButton">Мои цели (2)</h2>
+									<h2 class="mdlst-h2t-goals withButton">Мои цели (<?=count($goals)?>)</h2>
 									 <a class="goal-done-button mdlst-button withHeader" href="#">+ Добавить новую цель</a>
 
 								<div class="clear"></div>
@@ -33,11 +33,16 @@ echo $this->render('_panel.php');
 								</div>
     						</div> 
 
+
+							<?php foreach($goals as $goal) { ?> 
+
+							<a class="mygoals-block" href="<?=Yii::$app->urlManager->createUrl(['personal/goal-detail', 'goal_id' => $goal->goal_id])?>">
+
 							<div class="goal-content">
    								 <div class="mygoals-name-div">
                                      <div class="input-check withList">
 									    <input type="checkbox" id="ListbGoal1" name="ListbGoal1" value="ListbGoal1"/>
-										<label for="ListbGoal1" class="subtask-top-name" id="mygoals-name">Купить квартиру в Кудрово</label>
+    										<label for="ListbGoal1" class="subtask-top-name" id="mygoals-name"><?=$goal->name?></label>
 									</div>
 									 <div class=""></div>
 								</div>
@@ -50,45 +55,163 @@ echo $this->render('_panel.php');
 									 <div class="mygoals-left-wrapper">
 										 <span class="mygoals-diff">Сложность</span>
 										 <div class="mygoals-stars">
-											 <div class="mygoals-star star-yellow"></div>
-											 <div class="mygoals-star star-yellow"></div>
-											 <div class="mygoals-star star-grey"></div>
+											<?
+											for($n=1;$n<=$goal->difficulty;$n++){
+												 echo'<div class="mygoals-star star-yellow"></div>';
+											}
+											for($n=$goal->difficulty;$n<3;$n++){
+												 echo'<div class="mygoals-star star-grey"></div>';
+											}
+											
+											?>
 										</div>
 										 <span class="mygoals-dead">Дедлайн</span>
-										 <span class="mygoals-dead color-red">10.01.2017</span>
+										 <span class="mygoals-dead color-red"><? echo date("Y.m.d",strtotime($goal->deadline))?></span>
 
-										<div class="mygoals-lock"><img src="/template/img/goals/lock.png" alt=""/></div>
-										 <span class="mygoals-privat">Приватная</span>
+
+											<?
+											if($goal->private){
+												 echo'
+													<div class="mygoals-lock"><img src="/template/img/goals/lock.png" alt=""/></div>
+													 <span class="mygoals-privat">Приватная</span>';
+											}?>
 
 										<div class="mygoals-progress withProcess">
 											<div class="interests-selector-scale-viewport userpanel-info-scale-scale">
-										 		<div class="interests-selector-scale-track" style="margin-left: -80%;"></div>
+										 		<div class="interests-selector-scale-track" style="margin-left: -<?=100-$goal->percent_completed?>%;"></div>
 										 	</div>
 									 	</div>
 
+											<?
+											if($goal->active){
+												 echo'
 										<div class="processWprogress">
 											<div class="mygoals-clock"><img src="/template/img/goals/clock.png" alt=""></div>
 											 <span class="mygoals-process">В процессе</span>
-										</div>
+										</div>';
+											}?>
+
 									</div>
 								</div>
 
 								<div class="goalDescr goals-description">
-									<p>Награды подтверждают выдающи	еся достижения. У нас есть обширный каталог наград, а есть и секретные, о которых никто не знает.</p>
-									<p>Награды подтверждают выдающи	еся достижения. У нас есть обширный каталог наград, а есть и секретные, о которых никто не знает.</p>
+									<?=$goal->description?>
 								</div>
 
 							</div><!--listGoals-left-->
 
-							<div class="goals-picture-mid"><img src="/template/img/goals/goal-pic-mid.png" alt=""/></div>
+							<div class="goals-picture-mid"><img src="uploads<?=$goal->getPhotos()[0]->filename?>" alt=""/></div>
 
 							<div class="clear"></div>
 
-							<span class="showSubtasts">Смотреть подцели (5)</span>
+							<span class="showSubtasts">Смотреть подцели (<?=count($goal->getGoalSubtasks())?>)</span>
 							
+											<div>
+												<ul class="goal-subtask"> 
+												<? 
+												        $goal_subtasks = $goal->getGoalSubtasks();
+														var_dump($goal_subtasks);
+														foreach($goal_subtasks as $subtask){?>
+												        <?//=$subtask->goal_id?>
+														<li class="subtask-container">
+															<div class="subtask-top">
+																<div class="subtask-top-left subtask-done">
+                                                                    <div class="input-check">
+																	    <input checked="checked" type="checkbox" id="subGoal1" name="subGoal1" value="subGoal1"/>
+																		<label for="subGoal1" class="subtask-top-name">1. Выбрать дату с Мариной</label>
+																	</div>
+																	<div class="subtask-progress">
+																		<div class="interests-selector-scale-viewport userpanel-info-scale-scale subtask-progress-height">
+																	 		<div class="interests-selector-scale-track subtask-progress-height" style="margin-left: -80%;"></div>
+																	 	</div>
+																 	</div>
+																	 <span class="mygoals-dead color-red subtask-top-dead">10.01.2017</span>
+																</div>
+		
+
+															</div>
+															<div class="subtask-bottom">
+																Лучше всего летом, так как в городе не очень хочется...
+															</div>
+
+													</li>
+
+												<?}?>
+													<li class="subtask-container">
+															<div class="subtask-top">
+																<div class="subtask-top-left">
+                                                                    <div class="input-check">
+																	    <input type="checkbox" id="subGoal2" name="subGoal2" value="subGoal2"/>
+																		<label for="subGoal2" class="subtask-top-name">1. Выбрать дату с Мариной</label>
+																 	</div>
+
+																	<div class="subtask-progress">
+																		<div class="interests-selector-scale-viewport userpanel-info-scale-scale subtask-progress-height">
+																	 		<div class="interests-selector-scale-track subtask-progress-height" style="margin-left: -80%;"></div>
+																	 	</div>
+																 	</div>
+																	 <span class="mygoals-dead color-red subtask-top-dead">10.01.2017</span>
+																</div>
+		
+
+															</div>
+															<div class="subtask-bottom">
+																Лучше всего летом, так как в городе не очень хочется...
+															</div>
+															<ul class="subtask-points">
+																<li>
+																	<div class="input-check">
+																	    <input type="checkbox" id="subGoal2p1" name="subGoal2p1" value="subGoal2p1"/>
+																		<label for="subGoal2p1" class="subtask-top-name">1. Выбрать дату с Мариной</label>
+																	</div>
+																	<div class="clear"></div>
+																</li>
+																<li>
+                                                                    <div class="input-check">
+																	    <input type="checkbox" id="subGoal2p2" name="subGoal2p2" value="subGoal2p2"/>
+																		<label for="subGoal2p2" class="subtask-top-name">1. Выбрать дату с Мариной</label>
+																	</div>
+																	<div class="clear"></div>
+																</li>
+
+															</ul>
+													</li>
+
+
+													<li class="subtask-container">
+															<div class="subtask-top">
+																<div class="subtask-top-left subtask-done">
+                                                                    <div class="input-check">
+																	    <input checked="checked" type="checkbox" id="subGoal3" name="subGoal3" value="subGoal3"/>
+																		<label for="subGoal3" class="subtask-top-name">1. Выбрать дату с Мариной</label>
+																 	</div>
+																	<div class="subtask-progress">
+																		<div class="interests-selector-scale-viewport userpanel-info-scale-scale subtask-progress-height">
+																	 		<div class="interests-selector-scale-track subtask-progress-height" style="margin-left: -80%;"></div>
+																	 	</div>
+																 	</div>
+																	 <span class="mygoals-dead color-red subtask-top-dead">10.01.2017</span>
+																</div>
+		
+
+															</div>
+																	<div class="clear"></div>
+
+															<div class="subtask-bottom">
+																Лучше всего летом, так как в городе не очень хочется...
+															</div>
+
+													</li>
+
+
+
+												</ul>
+											</div>
+
+
 						</div><!--goal-content-->
-
-
+						</a>
+							<? }  ?> 
 
 							<div class="goal-content">
 
