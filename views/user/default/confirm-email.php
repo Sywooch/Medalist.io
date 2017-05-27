5bd8350375f2e7b24b2251439f7d31ead0a8adf9
Andrey <bikeway@yandex.ru>
2017-05-27 11:54:50 +0300

    mail

--- views/user/default/confirm-email.php
+++ views/user/default/confirm-email.php
@@ -23,7 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <div class="wc wc-c">
                     <div class="pregister-img"><img src="img/postman.png" alt=""></div>
                     <h2 class="mdlst-h2">Первый шаг сделан</h2>
-                    <h3 class="mdlst-h3">Письмо с кодом активации отправлено на электронную <br>почту <a href="#">petergromov@ya.ru</a></h3>
+                    <h3 class="mdlst-h3">Письмо с кодом активации отправлено на указанную электронную почту.</h3>
                      
                 </div>
             </div>
